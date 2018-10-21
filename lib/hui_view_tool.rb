4dbadbbb6a83851a2d3e3beef0d9b8792898882e
Kevin Hui <kevinhuitaylors@gmail.com>
2018-10-21 16:13:23 +0800

    huitool initial create

---
+++ lib/hui_view_tool.rb
@@ -0,0 +1,6 @@
+require "hui_view_tool/version"
+require "hui_view_tool/renderer"
+
+module HuiViewTool
+  # Your code goes here...
+end
---
+++ lib/hui_view_tool/version.rb
@@ -0,0 +1,3 @@
+module HuiViewTool
+  VERSION = "0.1.0"
+end
